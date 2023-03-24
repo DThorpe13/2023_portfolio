493ed166101aa213ebf8299f8afc00e16e6515f0
Derek Thorpe <derekthorpe13@gmail.com>
2023-03-23 20:12:29 -0400

    updated contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,12 +8,12 @@ const contact = () => {
             <div className='columns-container contact-sec-container'>
                 <div className='col-lg-6 col-xs-12 contact-col-left'>
                     <p className='contact-chat-para'>Lets Chat.<br />Tell me about your project.</p>
-                    <p>Lets create something together.</p>
-                    <p className='contact-mailer'>Email me at <br /> Derekthorpe13@gmail.com</p>
+                    <p className='contact-para-two'>Lets create something together.</p>
+                    <p className='contact-mailer'><span className='contact-email'>Email me @ </span><br /> <a href='mailto: derekthorpe13@gmail.com'>DerekThorpe13@gmail.com</a></p>
                 </div>
                 <div className='col-lg-6 col-xs-12 contact-col-right'>
                     <div>
-                        <p>Send a message.</p>
+                        
                         
                     </div>
                 </div>
