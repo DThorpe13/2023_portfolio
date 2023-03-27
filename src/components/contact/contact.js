c8875896fd694c23eecac8943ada2263bcdb6386
Derek Thorpe <derekthorpe13@gmail.com>
2023-03-27 13:36:41 -0400

    mobile fixes for contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,7 +8,7 @@ const contact = () => {
             <div className='columns-container contact-sec-container'>
                 <div className='col-lg-6 col-xs-12 contact-col-left'>
                     <p className='contact-chat-para'>Lets Chat.<br />Tell me about your project.</p>
-                    <p className='contact-para-two'>Lets create something together.</p>
+                    <p className='contact-para-two'><i>Lets create something together.</i></p>
                     <p className='contact-mailer'><span className='contact-email'>Email me @ </span><br /> <a href='mailto: derekthorpe13@gmail.com'>DerekThorpe13@gmail.com</a></p>
                 </div>
                 <div className='col-lg-6 col-xs-12 contact-col-right'>
